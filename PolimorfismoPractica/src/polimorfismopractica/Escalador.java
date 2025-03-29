62ab32ef930185f458e47df40e69cabb7ff88dd2
juanpablou22 <juanpabloum25@gmail.com>
2025-03-28 19:38:27 -0500

    Creaccion de la clase escalador

---
+++ PolimorfismoPractica/src/polimorfismopractica/Escalador.java
@@ -0,0 +1,41 @@
+package polimorfismopractica;
+
+public abstract class Escalador extends Ciclista {
+    private float aceleracionPromedio;
+    private float gradoRampa;
+
+    public Escalador(int id, String nombre, float aceleracionPromedio, float gradoRampa) {
+        super(id, nombre);
+        this.aceleracionPromedio = aceleracionPromedio;
+        this.gradoRampa = gradoRampa;
+    }
+
+    public float getAceleracionPromedio() {
+        return aceleracionPromedio;
+    }
+
+    public void setAceleracionPromedio(float aceleracionPromedio) {
+        this.aceleracionPromedio = aceleracionPromedio;
+    }
+
+    public float getGradoRampa() {
+        return gradoRampa;
+    }
+
+    public void setGradoRampa(float gradoRampa) {
+        this.gradoRampa = gradoRampa;
+    }
+    
+    @Override
+    public String imprimirTipo() {
+        return "Escalador";
+    }
+
+    @Override
+    public void imprimirDatos() {
+        System.out.println("ID: " + getIdentificador());
+        System.out.println("Nombre: " + getName());
+        System.out.println("Aceleración Promedio: " + aceleracionPromedio);
+        System.out.println("Grado de Rampa: " + gradoRampa);
+    }
+}

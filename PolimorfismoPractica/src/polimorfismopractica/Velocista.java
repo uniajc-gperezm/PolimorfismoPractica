6ac367bccf9377162d6644719486361816db63a1
HeleOficial <juanledesmaidarraga@gmail.com>
2025-03-28 19:35:08 -0500

    se implementa clase velocista

--- PolimorfismoPractica/src/polimorfismopractica/Velocista.java
+++ PolimorfismoPractica/src/polimorfismopractica/Velocista.java
@@ -8,6 +8,41 @@ package polimorfismopractica;
  *
  * @author g.perezmoreno
  */
-public class Velocista {
-    
+
+
+public class Velocista extends Ciclista {
+    private double potenciaPromedio;
+    private double velocidadPromedioSprint;
+
+    public Velocista(int identificador, String nombre, double potenciaPromedio, double velocidadPromedioSprint) {
+        super(identificador, nombre);
+        this.potenciaPromedio = potenciaPromedio;
+        this.velocidadPromedioSprint = velocidadPromedioSprint;
+    }
+
+    public double getPotenciaPromedio() {
+        return potenciaPromedio;
+    }
+
+    public void setPotenciaPromedio(double potenciaPromedio) {
+        this.potenciaPromedio = potenciaPromedio;
+    }
+
+    public double getVelocidadPromedioSprint() {
+        return velocidadPromedioSprint;
+    }
+
+    public void setVelocidadPromedioSprint(double velocidadPromedioSprint) {
+        this.velocidadPromedioSprint = velocidadPromedioSprint;
+    }
+
+    @Override
+    public String imprimirTipo() {
+        return "Es un Velocista";
+    }
+
+    @Override
+    public void imprimirDatos() {
+        super.imprimirDatos();
+    }
 }

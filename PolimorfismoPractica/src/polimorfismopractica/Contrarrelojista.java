aa48f5a9c0a53f01631425d00eaf32d8573fc5b7
estudiante103 <estudiante103@103-17.unicamacho.co>
2025-03-28 20:04:59 -0500

    se agrega clase contrarrelojista

---
+++ PolimorfismoPractica/src/polimorfismopractica/Contrarrelojista.java
@@ -0,0 +1,27 @@
+package polimorfismopractica;
+
+public class Contrarrelojista extends Ciclista{
+    private double velocidadMaxima;
+    
+    public Contrarrelojista(int identificador, String name, double velocidadMaxima){
+        super(identificador, name);
+        this.velocidadMaxima = velocidadMaxima;
+    }
+    
+    public double getVelocidadMaxima(){
+        return velocidadMaxima;
+    }
+    
+    public void setVelocidadMaxima(double velocidadMaxima){
+        this.velocidadMaxima = velocidadMaxima;
+    }
+    
+    public String imprimirTipo(){
+        return "Es un Contrarrelojista";
+    }
+    
+    public void imprimir(){
+        super.imprimirDatos();
+        System.out.println("Velocidad Maxima: " + velocidadMaxima + "km/h");
+    }
+}

524b942e21d99643f58c05571eaf92c6b5eb6962
steven <stev3np@gmail.com>
2025-03-31 14:24:49 -0500

    Se implementa clase velocista y se corrigen espacios.

--- PolimorfismoPractica/src/polimorfismopractica/Velocista.java
+++ PolimorfismoPractica/src/polimorfismopractica/Velocista.java
@@ -1,25 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package polimorfismopractica;
-
-/**
- *
- * @author g.perezmoreno
- */
-
-
 public class Velocista extends Ciclista {
     private double potenciaPromedio;
     private double velocidadPromedioSprint;
-
     public Velocista(int identificador, String nombre, double potenciaPromedio, double velocidadPromedioSprint) {
         super(identificador, nombre);
         this.potenciaPromedio = potenciaPromedio;
         this.velocidadPromedioSprint = velocidadPromedioSprint;
     }
-
     public double getPotenciaPromedio() {
         return potenciaPromedio;
     }
